6d741db19a7f3a45b26d012ce7d1b82645aac847
Jack Overby <jroverby92@gmail.com>
2020-01-12 19:33:46 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,11 +22,11 @@ def bonus
 
   #code your solution here:
   
-  epic_tragedy[:montague][:hero_friends][1], epic_tragedy[:montague][:heroine_friends][0] = 
-  epic_tragedy[:montague][:heroine_friends][0], epic_tragedy[:montague][:hero_friends][1]
+  epic_tragedy[:montague][:hero_friends][1], epic_tragedy[:capulet][:heroine_friends][0] = 
+  epic_tragedy[:capulet][:heroine_friends][0], epic_tragedy[:montague][:hero_friends][1]
   
   epic_tragedy[:montague][:hero][:status] = "dead"
-  epic_tragedy[:montague][:heroine][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
