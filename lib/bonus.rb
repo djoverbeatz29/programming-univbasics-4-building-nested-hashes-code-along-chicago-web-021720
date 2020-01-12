8b79705bec7dd55928e4fb05a4aecd5ed30c258d
Jack Overby <jroverby92@gmail.com>
2020-01-12 19:31:25 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -22,7 +22,8 @@ def bonus
 
   #code your solution here:
   
-  epic_tragedy[:montague][:hero_friends][1], epic_tragedy[:montague][:heroine_friends][0]
+  epic_tragedy[:montague][:hero_friends][1], epic_tragedy[:montague][:heroine_friends][0] = 
+  epic_tragedy[:montague][:heroine_friends][0] = epic_tragedy[:montague][:hero_friends][1]
 
   
 
